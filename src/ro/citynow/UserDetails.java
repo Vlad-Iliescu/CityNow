9355bb616db8a69af90f30a3baafed3ab0abcfe8
Vlad Iliescu <vlad@citynow.ro>
2013-04-10 23:21:10 +0300

    User details 0.2

--- src/ro/citynow/UserDetails.java
+++ src/ro/citynow/UserDetails.java
@@ -3,10 +3,17 @@ package ro.citynow;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ProgressBar;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class UserDetails extends Activity {
@@ -60,6 +67,40 @@ public class UserDetails extends Activity {
 
     private void updateDetalii(ServerResponse response) {
         Log.d("usr", String.valueOf(response.getResponseCode()));
+        if (response.getResponseCode() == 200) {
+            final JSONObject jsonObject;
+            try {
+                jsonObject = response.parseAsJson();
+                if (jsonObject != null) {
+                    Details details = new Details(this.userId);
+                    details.setDenumire(jsonObject.getString("d"));
+                    details.setTelefon(jsonObject.getString("t"));
+                    details.setWebsite(jsonObject.getString("w"));
+
+                    // adresa
+                    JSONObject adresaJson = jsonObject.getJSONObject("a");
+                    Address adresa = details.getAddress();
+                    adresa.setAdresa(adresaJson.getString("a"));
+                    adresa.setZona(adresaJson.getString("z"));
+                    adresa.setLatitudine(adresaJson.getString("la"));
+                    adresa.setLongitudine(adresaJson.getString("lo"));
+
+                    // galerie
+                    JSONArray poze = jsonObject.getJSONArray("g");
+                    if (poze.length() > 0) {
+                        ArrayList<Picture> pictures = details.getPictures();
+                        for (int i=0; i<poze.length(); ++i) {
+                            JSONObject poza = poze.getJSONObject(i);
+                            pictures.add(new Picture(poza.getInt("i"), poza.getString("d"), poza.getString("p"),
+                                    poza.getString("t")));
+                        }
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     //------------------------------------
@@ -70,15 +111,25 @@ public class UserDetails extends Activity {
         private Integer id;
         private String denumire;
         private String urlPic;
-        private String ulrThumb;
+        private String urlThumb;
         private Bitmap pic;
         private Bitmap thumb;
 
-        Picture(Integer id, String denumire, String urlPic, String ulrThumb) {
+        Picture(Integer id, String denumire, String urlPic, String urlThumb) {
             this.id = id;
             this.denumire = denumire;
             this.urlPic = urlPic;
-            this.ulrThumb = ulrThumb;
+            this.urlThumb = urlThumb;
+        }
+
+        public Drawable getThumbFromUrl() {
+            try {
+                InputStream is = (InputStream) new URL(urlThumb).getContent();
+                return Drawable.createFromStream(is, "CityNow");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 
@@ -86,23 +137,92 @@ public class UserDetails extends Activity {
         private String adresa;
         private String latitudine;
         private String longitudine;
+        private String zona;
 
-        Address(String adresa, String latitudine, String longitudine) {
+        Address(String adresa, String zona, String latitudine, String longitudine) {
             this.adresa = adresa;
             this.latitudine = latitudine;
             this.longitudine = longitudine;
+            this.zona = zona;
+        }
+
+        Address() {}
+
+        String getAdresa() {
+            return adresa;
+        }
+
+        void setAdresa(String adresa) {
+            this.adresa = adresa;
+        }
+
+        String getLatitudine() {
+            return latitudine;
+        }
+
+        void setLatitudine(String latitudine) {
+            this.latitudine = latitudine;
+        }
+
+        String getLongitudine() {
+            return longitudine;
+        }
+
+        void setLongitudine(String longitudine) {
+            this.longitudine = longitudine;
+        }
+
+        String getZona() {
+            return zona;
+        }
+
+        void setZona(String zona) {
+            this.zona = zona;
         }
     }
 
     class Details {
-        private Integer id;
+        private Long id;
         private String denumire;
-        private ArrayList<Picture> userList = new ArrayList<Picture>();
-        private Address address;
+        private ArrayList<Picture> pictures = new ArrayList<Picture>();
+        private Address address = new Address();
         private String telefon;
         private String website;
 
+        Details(Long id) {
+            this.id = id;
+        }
 
+        String getDenumire() {
+            return denumire;
+        }
+
+        void setDenumire(String denumire) {
+            this.denumire = denumire;
+        }
+
+        String getTelefon() {
+            return telefon;
+        }
+
+        void setTelefon(String telefon) {
+            this.telefon = telefon;
+        }
+
+        String getWebsite() {
+            return website;
+        }
+
+        void setWebsite(String website) {
+            this.website = website;
+        }
+
+        Address getAddress() {
+            return address;
+        }
+
+        ArrayList<Picture> getPictures() {
+            return pictures;
+        }
     }
-
 }
